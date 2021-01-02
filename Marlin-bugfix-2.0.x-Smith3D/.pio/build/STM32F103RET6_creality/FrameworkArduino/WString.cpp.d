547161e94e2e36d4887dca6d3c51d1d060fe5d92
Rui Santos <61985674+Santos-Rui@users.noreply.github.com>
2021-01-02 20:11:26 +0000

    wow

---
+++ Marlin-bugfix-2.0.x-Smith3D/.pio/build/STM32F103RET6_creality/FrameworkArduino/WString.cpp.d
@@ -0,0 +1,14 @@
+.pio\build\STM32F103RET6_creality\FrameworkArduino\WString.cpp.o: \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\WString.cpp \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\WString.h \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple/avr/pgmspace.h \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\itoa.h \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\avr/dtostrf.h
+
+C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\WString.h:
+
+C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple/avr/pgmspace.h:
+
+C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\itoa.h:
+
+C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\avr/dtostrf.h:
---
+++ Marlin-bugfix-2.0.x-Smith3D/.pio/build/STM32F103RET6_creality/FrameworkArduino/itoa.c.d
@@ -0,0 +1,5 @@
+.pio\build\STM32F103RET6_creality\FrameworkArduino\itoa.c.o: \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\itoa.c \
+ C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\itoa.h
+
+C:\Users\RucaWin10\.platformio\packages\framework-arduinoststm32-maple\STM32F1\cores\maple\itoa.h:
