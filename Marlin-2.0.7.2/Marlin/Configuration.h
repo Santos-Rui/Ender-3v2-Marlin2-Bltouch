c92d44a6852f72a27c18e53b6bbe3b8d088f7931
Rui Santos <61985674+Santos-Rui@users.noreply.github.com>
2021-06-01 15:16:38 +0100

    steppers never off?

--- Marlin-2.0.7.2/Marlin/Configuration.h
+++ Marlin-2.0.7.2/Marlin/Configuration.h
@@ -1500,15 +1500,29 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 140
+
+
+#define PREHEAT_1_LABEL       "Bed60"
+#define PREHEAT_1_TEMP_HOTEND 0
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED   0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED     70
-#define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
+
+#define PREHEAT_2_LABEL       "PLA"
+#define PREHEAT_2_TEMP_HOTEND 150
+#define PREHEAT_2_TEMP_BED     60
+#define PREHEAT_2_FAN_SPEED   0 // Value from 0 to 255
+
+#define PREHEAT_3_LABEL       "Bed 80"
+#define PREHEAT_3_TEMP_HOTEND 0
+#define PREHEAT_3_TEMP_BED    80
+#define PREHEAT_3_FAN_SPEED   0 // Value from 0 to 255
+
+
+#define PREHEAT_4_LABEL       "PETG"
+#define PREHEAT_4_TEMP_HOTEND 220
+#define PREHEAT_4_TEMP_BED     80
+#define PREHEAT_4_FAN_SPEED   0 // Value from 0 to 255
 
 /**
  * Nozzle Park
